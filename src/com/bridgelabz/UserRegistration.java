7d3020bcabee1c39f67f64602ce3358af9aa4d9a
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-04 17:46:24 -0800

    Added: User Need To Enter Valid First Name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,9 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class UserRegistration {
+	private static final String NamePattern = "^[A-Z]{1}[a-z]{2,}$"; //pattern is fixed and final (static final)
+	Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		System.out.println("Welcome To User Registration Program");
+	public void FirstnameValidator() {
+		System.out.println("Enter First Name");
+		String input = scanner.next();
+		if (input.matches(NamePattern)) {   //if entered name is matching with pattern then true
+			System.out.println("first name sucessfully entered");
+		} else {
+			System.out.println("**Invalid First name**try again**");
+			FirstnameValidator();
+		}
 	}
-
 }
